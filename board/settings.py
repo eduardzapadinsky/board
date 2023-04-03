b900b9f5c5771bcbe26fd88480a3e27639a1d6bf
Zapadynsky <e.zapadinsky@gmail.com>
2023-04-03 17:07:59 +0200

    correct css, html

--- board/settings.py
+++ board/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     # Middleware to expire sessions after specific amount of time
-    'board.logout_middleware.SessionTimeoutMiddleware',
+    # 'board.logout_middleware.SessionTimeoutMiddleware',
 ]
 
 ROOT_URLCONF = 'board.urls'
